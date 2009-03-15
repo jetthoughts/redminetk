c60560d4e8e44b878d8ed1885b8efec92571d530
Michael Nikitochkin <michael@jetthoughts.com>
2009-03-15 21:15:12 +0200

    ---

--- src/models/rest.rb
+++ src/models/rest.rb
@@ -1,8 +1,5 @@
 class Rest < ActiveResource::Base
-#  self.site = "http://localhost:3000"
   self.site = "http://redmine.railsware.com"
-#  self.user=("miry")
-  self.user=("michael.nikitochkin")
-#  self.password=("aries")
-  self.password=("bc897462")
+  self.user=("your_user_name")
+  self.password=("your_password")
 end
